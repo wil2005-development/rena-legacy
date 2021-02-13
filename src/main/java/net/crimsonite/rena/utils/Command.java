2c853da1f2659d4aaa6582295313a8458a38c25e
Nhalrath <akira.yami@yahoo.com>
2021-02-13 14:06:31 +0800

    Implement cooldown system

--- src/main/java/net/crimsonite/rena/utils/Command.java
+++ src/main/java/net/crimsonite/rena/utils/Command.java
@@ -1,30 +1,73 @@
 package net.crimsonite.rena.utils;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
 import net.crimsonite.rena.RenaBot;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public abstract class Command extends ListenerAdapter {
 	
+	private ConcurrentHashMap<String, Long> cooldownCache = new ConcurrentHashMap<>();
+	
 	public abstract void execute(MessageReceivedEvent event, String[] args);
 	public abstract String getCommandName();
+	public abstract long cooldown();
 	
 	// Reserved for future use
 	public abstract boolean isOwnerCommand();
 	
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
-		if (event.getAuthor().isBot()) {
+		User author = event.getAuthor();
+		
+		if (author.isBot()) {
 			return;
 		}
-        if (containsCommand(event.getMessage())) {
-        	execute(event, commandArgs(event.getMessage()));
-        }
+		
+		if (cooldownCache.containsKey(author.getId())) {
+			long remainingCooldownShortened = remainingCooldown(author.getId())-TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+			
+			if (remainingCooldownShortened > 0) {
+				event.getChannel().sendMessageFormat("**Oi oi! Slow down!!!** *This command is on cooldown for* `%d`s.", remainingCooldownShortened).queue();
+				
+				return;
+			}
+			else if (remainingCooldownShortened <= 0) {
+				removeCooldown(author.getId());
+				
+				if (containsCommand(event.getMessage())) {
+		        	execute(event, commandArgs(event.getMessage()));
+		        	setCooldown(author.getId());
+		        }
+			}
+		}
+		else {
+			if (containsCommand(event.getMessage())) {
+	        	execute(event, commandArgs(event.getMessage()));
+	        	setCooldown(author.getId());
+	        }
+		}
 	}
 	
+	protected long remainingCooldown(String UID) {
+		return cooldownCache.get(UID);
+	}
+	
+	protected void setCooldown(String UID) {
+		long cooldownDuration = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) + this.cooldown();
+		cooldownCache.put(UID, cooldownDuration);
+	}
+	
+	protected void removeCooldown(String UID) {
+		cooldownCache.remove(UID);
+	}
+
 	protected boolean containsCommand(Message message) {
 		return (RenaBot.prefix+getCommandName()).equalsIgnoreCase(commandArgs(message)[0]);
 	}
