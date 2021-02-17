c9f2928be2d6b5b5f0f2c6496aa8bb558530a12a
Nhalrath <akira.yami@yahoo.com>
2021-02-17 17:53:38 +0800

    add levelup handler

--- src/main/java/net/crimsonite/rena/engine/RoleplayEngine.java
+++ src/main/java/net/crimsonite/rena/engine/RoleplayEngine.java
@@ -4,25 +4,26 @@ import net.crimsonite.rena.database.DBUsers;
 
 public class RoleplayEngine {
 	
-	public class commenceBattle {
-		String player;
-		String enemy;
-		int playerHP;
-		int enemyHP;
-		int playerMP;
-		int enemyMP;
-		int playerDef;
-		int enemyDef;
-		int playerAttk;
-		int enemyAttk;
+	public class CommenceBattle {
 		
-		public int attack(String playerID) {
-			this.player = playerID;
-			
-			int playerHP = Integer.parseInt(DBUsers.getValueString(player, "HP"));
-			int playerMP = Integer.parseInt(DBUsers.getValueString(player, "MP"));
-			int playerDef = Integer.parseInt(DBUsers.getValueString(player, "Def"));
-			int playerAttk = Integer.parseInt(DBUsers.getValueString(player, "Attk"));
+		private String player;
+		private String enemy;
+		private int playerHP;
+		private int enemyHP;
+		private int playerMP;
+		private int enemyMP;
+		private int playerDef;
+		private int enemyDef;
+		private int playerAttk;
+		private int enemyAttk;
+		private int playerLevel;
+		private int playerExp;
+		
+		public int attack(String player) {
+			playerHP = Integer.parseInt(DBUsers.getValueString(player, "HP"));
+			playerMP = Integer.parseInt(DBUsers.getValueString(player, "MP"));
+			playerDef = Integer.parseInt(DBUsers.getValueString(player, "Def"));
+			playerAttk = Integer.parseInt(DBUsers.getValueString(player, "Attk"));
 			
 			while (playerHP > 0 || enemyHP > 0) {
 				// TODO Add complexity. The code below is just a demonstration on how the value should be returned.
@@ -31,6 +32,33 @@ public class RoleplayEngine {
 			}
 			
 			return 0; // The outcome
+		}
+		
+		private boolean checkExp(int level, int exp) {
+			int nextLevel = level += 1;
+			int requiredExpForNextLevel = 50*nextLevel*(nextLevel+1);
+			
+			if (exp >= requiredExpForNextLevel) {
+				return true;
+			}
+			
+			return false;
+		}
+		
+		public void handleLevelup(String player) {
+			playerLevel = Integer.parseInt(DBUsers.getValueString(player, "level"));
+			playerExp = Integer.parseInt(DBUsers.getValueString(player, "exp"));
+			
+			boolean increment = checkExp(playerLevel, playerExp);
+			
+			if (increment) {
+				while (increment) {
+					playerLevel += 1;
+					increment = checkExp(playerLevel, playerExp);
+				}
+				
+				DBUsers.incrementValue(player, "level", playerLevel);
+			}
 		}
 		
 	}
