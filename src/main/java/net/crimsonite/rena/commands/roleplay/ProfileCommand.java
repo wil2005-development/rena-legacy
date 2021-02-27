839a6e93eb6daa05c4fdb3764854aa85720eac01
Nhalrath <akira.yami@yahoo.com>
2021-02-27 18:30:25 +0800

    add finder util + optimizations

--- src/main/java/net/crimsonite/rena/commands/roleplay/ProfileCommand.java
+++ src/main/java/net/crimsonite/rena/commands/roleplay/ProfileCommand.java
@@ -1,64 +1,77 @@
 package net.crimsonite.rena.commands.roleplay;
 
 import java.awt.Color;
+import java.util.List;
+
+import com.jagrosh.jdautilities.commons.utils.FinderUtil;
 
 import net.crimsonite.rena.database.DBUsers;
 import net.crimsonite.rena.utils.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class ProfileCommand extends Command {
+	
+	private static void sendEmbed(MessageReceivedEvent event, User user) {
+		User author = event.getAuthor();
+		Color roleColor = event.getGuild().retrieveMember(user).complete().getColor();
+		EmbedBuilder embed = new EmbedBuilder()
+				.setColor(roleColor)
+				.setTitle(user.getName() + "'s Profile")
+				.setThumbnail(user.getEffectiveAvatarUrl())
+				.addField("Level", DBUsers.getValueString(user.getId(), "level"), false)
+				.addField("Exp", DBUsers.getValueString(user.getId(), "exp"), true)
+				.addField("Rep", DBUsers.getValueString(user.getId(), "rep"), true)
+				.addField("Money", DBUsers.getValueString(user.getId(), "money"), true)
+				.setFooter(user.getName(), user.getEffectiveAvatarUrl());
+		
+		event.getChannel().sendMessage(embed.build()).queue();
+	}
 
 	@Override
 	public void execute(MessageReceivedEvent event, String[] args) {
 		MessageChannel channel = event.getChannel();
-		User author = event.getAuthor();
 		
-		if (event.getMessage().getMentionedUsers().isEmpty()) {
+		if (args.length == 1) {
 			try {
-				Color roleColor = event.getGuild().retrieveMember(author).complete().getColor();
-				EmbedBuilder embed = new EmbedBuilder()
-						.setColor(roleColor)
-						.setTitle(author.getName() + "'s Profile")
-						.setThumbnail(author.getEffectiveAvatarUrl())
-						.addField("Level", DBUsers.getValueString(author.getId(), "LEVEL"), false)
-						.addField("Exp", DBUsers.getValueString(author.getId(), "EXP"), true)
-						.addField("Rep", DBUsers.getValueString(author.getId(), "REP"), true)
-						.addField("Money", DBUsers.getValueString(author.getId(), "MONEY"), true)
-						.addField("Hp", DBUsers.getValueString(author.getId(), "HP"), true)
-						.addField("Mp", DBUsers.getValueString(author.getId(), "MP"), true)
-						.addField("Atk", DBUsers.getValueString(author.getId(), "ATK"), true)
-						.addField("Def", DBUsers.getValueString(author.getId(), "DEF"), true)
-						.setFooter(author.getName(), author.getEffectiveAvatarUrl());
-				
-				channel.sendMessage(embed.build()).queue();
+				sendEmbed(event, event.getAuthor());
 			}
 			catch (NullPointerException ignored) {
 				DBUsers.registerUser(event.getAuthor().getId());
 				channel.sendMessage("Oops! Try again?").queue();
 			}
 		}
-		else {
-			try {
-				User user = event.getMessage().getMentionedUsers().get(0);
-				Color roleColor = event.getGuild().retrieveMember(user).complete().getColor();
-				EmbedBuilder embed = new EmbedBuilder()
-						.setColor(roleColor)
-						.setTitle(user.getName() + "'s Profile")
-						.setThumbnail(user.getEffectiveAvatarUrl())
-						.addField("Level", DBUsers.getValueString(user.getId(), "level"), false)
-						.addField("Exp", DBUsers.getValueString(user.getId(), "exp"), true)
-						.addField("Rep", DBUsers.getValueString(user.getId(), "rep"), true)
-						.addField("Money", DBUsers.getValueString(user.getId(), "money"), true)
-						.setFooter(user.getName(), user.getEffectiveAvatarUrl());
-				
-				channel.sendMessage(embed.build()).queue();
+		else if (args.length >= 2) {
+			if (!event.getMessage().getMentionedMembers().isEmpty()) {
+				try {
+					User user = event.getMessage().getMentionedUsers().get(0);
+					sendEmbed(event, user);
+				}
+				catch (NullPointerException ignored) {
+					DBUsers.registerUser(event.getMessage().getMentionedUsers().get(0).getId());
+					channel.sendMessage("Oops! Try again?").queue();
+				}
 			}
-			catch (NullPointerException ignored) {
-				DBUsers.registerUser(event.getMessage().getMentionedUsers().get(0).getId());
-				channel.sendMessage("Oops! Try again?").queue();
+			else {
+				List<Member> listedMembers = FinderUtil.findMembers(args[1], event.getGuild());
+				
+				if (listedMembers.isEmpty()) {
+					channel.sendMessage("*Err... I can't find that person. Try doing it again, I might've missed them*").queue();
+					event.getGuild().loadMembers();
+				}
+				else {
+					try {
+						User user = listedMembers.get(0).getUser();
+						sendEmbed(event, user);
+					}
+					catch (NullPointerException ignored) {
+						DBUsers.registerUser(listedMembers.get(0).getId());
+						channel.sendMessage("Oops! Try again?").queue();
+					}
+				}
 			}
 		}
 	}
