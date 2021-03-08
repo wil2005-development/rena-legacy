a3c8f34a81de798f3e84a81003352324e5b3c42f
Nhalrath <akira.yami@yahoo.com>
2021-03-08 14:49:56 +0800

    rename DBUsers to DBReadWrite

--- src/main/java/net/crimsonite/rena/database/DBReadWrite.java
+++ src/main/java/net/crimsonite/rena/database/DBReadWrite.java
@@ -20,23 +20,35 @@ package net.crimsonite.rena.database;
 import java.util.HashMap;
 
 import com.rethinkdb.RethinkDB;
-import com.rethinkdb.gen.ast.Table;
+import com.rethinkdb.gen.ast.Db;
 import com.rethinkdb.net.Connection;
 
-public class DBUsers {
+public class DBReadWrite {
 	
-	private static Table user = DBConnection.users();
+	private static Db db = DBConnection.db();
 	private static final RethinkDB r = RethinkDB.r;
 	private static final Connection conn = DBConnection.conn();
+	
+	public enum Table {
+		USERS("users"),
+		GUILDS("users");
+
+		public final String TABLE_NAME;
+		
+		Table(String TABLE_NAME) {
+			this.TABLE_NAME = TABLE_NAME;
+		}
+	}
 	
 	/**
 	 * (Write)Registers the user to the database when called.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 */
 	public static void registerUser(String UID) {
 		// Roleplay stats are uppercased on purpose.
-		user.insert(r.array(
+		db.table(Table.USERS.TABLE_NAME).insert(r.array(
 				r.hashMap("id", UID)
 				.with("LEVEL", 0)
 				.with("EXP", 0)
@@ -53,91 +65,97 @@ public class DBUsers {
 	/**
 	 * (Write)Increments an integer value from db.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to be incremented.
 	 * @param val -The amount to increment.
 	 * @throws IllegalArgumentException If the provided value is a negative number
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static void incrementValue(String UID, String key, int val) throws IllegalArgumentException, NullPointerException {
+	public static void incrementValue(Table table, String UID, String key, int val) throws IllegalArgumentException, NullPointerException {
 		if (val <= 0) {
 			throw new IllegalArgumentException("Value cannot be a negative number (%d).".formatted(val));
 		}
 		
-		HashMap<String, String> obj = user.get(UID).run(conn);
+		HashMap<String, String> obj = db.table(table.TABLE_NAME).get(UID).run(conn);
 		int initialValue = Integer.parseInt(String.valueOf(obj.get(key)));
 		int incrementedValue = initialValue += val;
 		
-		user.get(UID).update(r.hashMap(key, incrementedValue)).runNoReply(conn);
+		db.table(table.TABLE_NAME).get(UID).update(r.hashMap(key, incrementedValue)).runNoReply(conn);
 	}
 	
 	/**
 	 * (Write)Decrements an integer value from db.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to be decremented.
 	 * @param val -The amount to decrement.
 	 * @throws IllegalArgumentException If the provided value is a negative number
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static void decrementValue(String UID, String key, int val) throws IllegalArgumentException, NullPointerException {
+	public static void decrementValue(Table table, String UID, String key, int val) throws IllegalArgumentException, NullPointerException {
 		if (val <= 0) {
 			throw new IllegalArgumentException("Value cannot be a negative number (%d).".formatted(val));
 		}
 		
-		HashMap<String, String> obj = user.get(UID).run(conn);
+		HashMap<String, String> obj = db.table(table.TABLE_NAME).get(UID).run(conn);
 		int initialValue = Integer.parseInt(String.valueOf(obj.get(key)));
 		int decrementedValue = initialValue -= val;
 		
-		user.get(UID).update(r.hashMap(key, decrementedValue)).runNoReply(conn);
+		db.table(table.TABLE_NAME).get(UID).update(r.hashMap(key, decrementedValue)).runNoReply(conn);
 	}
 	
 	/**
 	 * (Write)Modifies a String value from db.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -the db variable to be modified.
 	 * @param val -The value to exchange.
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static void modifyDataString(String UID, String key, String val) throws NullPointerException {
-		user.get(UID).update(r.hashMap(key, val)).runNoReply(conn);
+	public static void modifyDataString(Table table, String UID, String key, String val) throws NullPointerException {
+		db.table(table.TABLE_NAME).get(UID).update(r.hashMap(key, val)).runNoReply(conn);
 	}
 	
 	/**
 	 * (Write)Modifies an Int value from db.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to be modified.
 	 * @param val =The value to exchange.
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static void modifyDataInt(String UID, String key, int val) throws NullPointerException {		
-		user.get(UID).update(r.hashMap(key, val)).runNoReply(conn);
+	public static void modifyDataInt(Table table, String UID, String key, int val) throws NullPointerException {		
+		db.table(table.TABLE_NAME).get(UID).update(r.hashMap(key, val)).runNoReply(conn);
 	}
 	
 	/**
 	 * (Write)Modifies a Boolean value from db.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to be modified.
 	 * @param val -The value to exchange.
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static void modifyDataBoolean(String UID, String key, boolean val) throws NullPointerException {
-		user.get(UID).update(r.hashMap(key, val)).runNoReply(conn);
+	public static void modifyDataBoolean(Table table, String UID, String key, boolean val) throws NullPointerException {
+		db.table(table.TABLE_NAME).get(UID).update(r.hashMap(key, val)).runNoReply(conn);
 	}
 	
 	/**
 	 * (Read)Returns the String value of the specified key.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to get.
 	 * @return value of key
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static String getValueString(String UID, String key) throws NullPointerException {
-		HashMap<String, String> obj = user.get(UID).run(conn);
+	public static String getValueString(Table table, String UID, String key) throws NullPointerException {
+		HashMap<String, String> obj = db.table(table.TABLE_NAME).get(UID).run(conn);
 		
 		return String.valueOf(obj.get(key));
 	}
@@ -145,13 +163,14 @@ public class DBUsers {
 	/**
 	 * (Read)Returns the Integer value of the specified key.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to get.
 	 * @return value of key
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static int getValueInt(String UID, String key) throws NullPointerException {
-		HashMap<String, String> obj = user.get(UID).run(conn);
+	public static int getValueInt(Table table, String UID, String key) throws NullPointerException {
+		HashMap<String, String> obj = db.table(table.TABLE_NAME).get(UID).run(conn);
 		
 		return Integer.parseInt(obj.get(key));
 	}
@@ -159,13 +178,14 @@ public class DBUsers {
 	/**
 	 * (Read)Returns the Boolean value of the specified key.
 	 * 
+	 * @param table -The table to modify.
 	 * @param UID -The Unique ID of the user.
 	 * @param key -The db variable to get.
 	 * @return value of key
 	 * @throws NullPointerException If the user is not found in the database.
 	 */
-	public static Boolean getValueBoolean(String UID, String key) throws NullPointerException {
-		HashMap<String, String> obj = user.get(UID).run(conn);
+	public static Boolean getValueBoolean(Table table, String UID, String key) throws NullPointerException {
+		HashMap<String, String> obj = db.table(table.TABLE_NAME).get(UID).run(conn);
 		
 		return Boolean.parseBoolean(obj.get(key));
 	}
