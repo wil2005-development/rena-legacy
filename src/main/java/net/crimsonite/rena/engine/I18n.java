6f33d82e3694b9c53ebf7ad9fc61a96f2fea0774
Nhalrath <akira.yami@yahoo.com>
2021-03-12 21:12:47 +0800

    allow non-categorized messages to use i18n

--- src/main/java/net/crimsonite/rena/engine/I18n.java
+++ src/main/java/net/crimsonite/rena/engine/I18n.java
@@ -12,26 +12,58 @@ public class I18n {
 	
 	private static String defaultLanguage = "en";
 	private static String defaultCountry = "US";
+	
 	private static final Logger logger = LoggerFactory.getLogger(I18n.class);
 	
-	public static String getMessage(String language, String country, String context) {
+	/**
+	 * @param language
+	 * @param country
+	 * @param key -The key for message to look for.
+	 * @return message
+	 */
+	public static String getMessage(String language, String country, String key) {
 		String locale = "%s_%s".formatted(language, country);
 		String message = "NULL";
 		
-		ObjectMapper mapper = new ObjectMapper();
-		
 		try {
+			String[] keys = key.split("\\.");
+			String category;
+			String context;
+			String messageKey;
+			
+			ObjectMapper mapper = new ObjectMapper();
 			JsonNode resourceFile = mapper.readTree(I18n.class.getResourceAsStream("/languages/%s.json".formatted(locale)));
 			
-			message = resourceFile.get(context).asText();
-		} catch (IOException ignored) {
-			logger.warn("Message not found");
+			if (keys.length == 2) {
+				context = keys[0];
+				messageKey = keys[1];
+				
+				message = resourceFile.get(context).get(messageKey).asText();
+			}
+			else if (keys.length == 3) {
+				category = keys[0];
+				context = keys[1];
+				messageKey = keys[2];
+				
+				message = resourceFile.get(category).get(context).get(messageKey).asText();
+			}
+			
+		}
+		catch (ArrayIndexOutOfBoundsException ignored) {
+			logger.warn("Invalid key sequence (%s)".formatted(key));
+		}
+		catch (IOException ignored) {
+			logger.warn("Message not found.");
 		}
 		
 		return message;
 	}
 	
-	public static String getMessage(String context) {
-		return getMessage(defaultLanguage, defaultCountry, context);
+	/**
+	 * @param key -The key for message to look for.
+	 * @return message
+	 */
+	public static String getMessage(String key) {
+		return getMessage(defaultLanguage, defaultCountry, key);
 	}
 }
