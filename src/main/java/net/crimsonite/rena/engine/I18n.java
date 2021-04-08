04d408c33b51f6f24ef49821830712fd95557dd4
Nhalrath <akira.yami@yahoo.com>
2021-04-08 19:30:24 +0800

    add getStringArray function

--- src/main/java/net/crimsonite/rena/engine/I18n.java
+++ src/main/java/net/crimsonite/rena/engine/I18n.java
@@ -119,4 +119,89 @@ public class I18n {
 	public static String getMessage(String key) {
 		return getMessage(defaultLanguage, defaultCountry, key);
 	}
+	
+	/**
+	 * @param language
+	 * @param country
+	 * @param key -The key for list to look for.
+	 * @return message
+	 */
+	public static String[] getStringArray(String language, String country, String key) {
+		String locale = "%s_%s".formatted(language, country);
+		String[] list = new String[] {};
+		
+		try {
+			String[] keys = key.split("\\.");
+			String key1;
+			String key2;
+			String key3;
+			String messageKey;
+			
+			int keyLength = keys.length;
+			
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode resourceFile = mapper.readTree(I18n.class.getResourceAsStream("/languages/%s.json".formatted(locale)));
+			
+			switch (keyLength) {
+				case 2:
+					key1 = keys[0];
+					messageKey = keys[1];
+					
+					list = mapper.convertValue(resourceFile.get(key1).get(messageKey), String[].class);
+					
+					break;
+				case 3:
+					key1 = keys[0];
+					key2 = keys[1];
+					messageKey = keys[2];
+					
+					list = mapper.convertValue(resourceFile.get(key1).get(key2).get(messageKey), String[].class);
+					
+					break;
+				case 4:
+					key1 = keys[0];
+					key2 = keys[1];
+					key3 = keys[2];
+					messageKey = keys[3];
+					
+					list = mapper.convertValue(resourceFile.get(key1).get(key2).get(key3).get(messageKey), String[].class);
+					
+					break;
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException ignored) {
+			logger.warn("Invalid key sequence (%s)".formatted(key));
+		}
+		catch (IOException ignored) {
+			logger.warn("Message not found.");
+		}
+		
+		return list;
+	}
+	
+	/**
+	 * @param user -The UID of the user.
+	 * @param key -The key for list to look for.
+	 * @return
+	 */
+	public static String[] getStringArray(String user, String key) {
+		String language = defaultLanguage;
+		String country = defaultCountry;
+		
+		try {
+			language = DBReadWrite.getValueString(Table.USERS, user, "Language");
+			country = DBReadWrite.getValueString(Table.USERS, user, "Country");
+		}
+		catch (Exception | Error ignored) {}
+		
+		return getStringArray(language, country, key);
+	}
+	
+	/**
+	 * @param key -The key for list to look for.
+	 * @return message
+	 */
+	public static String[] getStringArray(String key) {
+		return getStringArray(defaultLanguage, defaultCountry, key);
+	}
 }
