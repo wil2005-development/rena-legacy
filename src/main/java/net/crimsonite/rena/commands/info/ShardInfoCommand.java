da34b31006c88d2d7bd0fd8a12458074984736b7
Nhalrath <akira.yami@yahoo.com>
2021-07-30 16:03:12 +0800

    Correct shard info

--- src/main/java/net/crimsonite/rena/commands/info/ShardInfoCommand.java
+++ src/main/java/net/crimsonite/rena/commands/info/ShardInfoCommand.java
@@ -19,11 +19,14 @@ package net.crimsonite.rena.commands.info;
 
 import java.awt.Color;
 
+import net.crimsonite.rena.RenaConfig;
 import net.crimsonite.rena.commands.Command;
 import net.crimsonite.rena.core.I18n;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDA.ShardInfo;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -41,28 +44,55 @@ public class ShardInfoCommand extends Command {
         ShardManager shardManager = jda.getShardManager();
 
         long totalUsers = 0;
+        long totalGuilds = 0;
+        long shardUsers = 0;
+        long shardGuilds = 0;
 
-        for (User user : shardManager.getUsers()) {
-            if (!user.isBot()) {
-                totalUsers++;
+        for (Guild guild : (shardManager != null ? shardManager.getGuilds() : jda.getGuilds())) {
+            for (Member member : guild.getMembers()) {
+                if (!member.getUser().isBot()) {
+                    totalUsers++;
+                }
             }
+
+            totalGuilds++;
+        }
+
+        for (Guild guild : jda.getGuilds()) {
+            for (Member member : guild.getMembers()) {
+                if (!member.getUser().isBot()) {
+                    shardUsers++;
+                }
+            }
+
+            shardGuilds++;
         }
 
         EmbedBuilder embed = new EmbedBuilder()
                 .setColor(roleColor)
                 .setTitle(I18n.getMessage(author.getId(), "info.shard_info.embed.title").formatted(jda.getShardInfo().getShardString()))
                 .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.shard_id"), "#" + shardInfo.getShardId(), false)
-                .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.average_gateway_ping"), "%dm/s".formatted(Math.round(shardManager.getAverageGatewayPing())), false)
-                .addBlankField(false)
-                .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_shards"), String.valueOf(shardManager.getShardsTotal()), true)
-                .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.online_shards"), String.valueOf(shardManager.getShardsRunning()), true)
-                .addBlankField(false)
-                .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_guilds"), String.valueOf(shardManager.getGuilds().size()), true)
-                .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_users"), String.valueOf(totalUsers), true)
                 .setFooter(author.getName(), author.getEffectiveAvatarUrl());
 
-        channel.sendMessageEmbeds(embed.build()).queue();
+        if (RenaConfig.isSharding() && (shardManager != null)) {
+            embed.addField(I18n.getMessage(author.getId(), "info.shard_info.embed.average_gateway_ping"), "%dm/s".formatted(Math.round(shardManager.getAverageGatewayPing())), false)
+                    .addBlankField(false)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_shards"), String.valueOf(shardManager.getShardsTotal()), true)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.online_shards"), String.valueOf(shardManager.getShardsRunning()), true)
+                    .addBlankField(false)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_guilds"), String.valueOf(totalGuilds), true)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.shard_guilds"), String.valueOf(shardGuilds), true)
+                    .addBlankField(false)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_users"), String.valueOf(totalUsers), true)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.shard_users"), String.valueOf(shardUsers), true);
+        }
+        else {
+            embed.addBlankField(false)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_guilds"), String.valueOf(totalGuilds), true)
+                    .addField(I18n.getMessage(author.getId(), "info.shard_info.embed.total_users"), String.valueOf(totalUsers), true);
+        }
 
+        channel.sendMessageEmbeds(embed.build()).queue();
     }
 
     @Override
